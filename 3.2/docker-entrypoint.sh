08a5861b74c383d369d8452d00ed8de9bd176d62
nikolaimerkel <merkelnikolai@googlemail.com>
2017-07-07 15:01:00 +0200

    Changed redis to 42

--- 3.2/docker-entrypoint.sh
+++ 3.2/docker-entrypoint.sh
@@ -9,7 +9,8 @@ fi
 
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
-	chown -R redis .
+	chown -R 42 .
+	echo "IF"
 	exec gosu redis "$0" "$@"
 fi
 
